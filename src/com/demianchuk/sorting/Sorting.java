7e8e6876db9c1c74126c1e0dfa473e8597cdf13e
Mykola Demianchuk <nicksetone@gmail.com>
2019-01-23 11:16:08 -0600

    feat: implement selection sort

--- src/com/demianchuk/sorting/Sorting.java
+++ src/com/demianchuk/sorting/Sorting.java
@@ -3,8 +3,8 @@ package com.demianchuk.sorting;
 public class Sorting {
     public static void main(String[] args) {
         int[] initial = {6, 7, 12, 4, 6, -2, 7, 3};
-        Sortable bubbleSort = new BubbleSort();
-        int[] sorted = bubbleSort.sort(initial);
+        Sortable selectionSort = new SelectionSort();
+        int[] sorted = selectionSort.sort(initial);
         SortHelper.print(sorted);
     }
 }
